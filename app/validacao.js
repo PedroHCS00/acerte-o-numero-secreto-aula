1993b8d9b954fcffe00909f0e0769dbd17b6e379
Pedro Carvalho <121904612+PedroHCS00@users.noreply.github.com>
2023-01-20 11:07:41 -0300

    Add files via upload
    Projeto do curso "Validações e reconhecimento de voz" utilizando JavaScript, HTML e CSS.

---
+++ app/validacao.js
@@ -0,0 +1,41 @@
+function VerificaChute(chute) {
+    const numero = +chute
+
+    if (chuteForInvalido(numero)) {
+        elementoChute.innerHTML += '<div>Valor inválido</div>'
+    }
+
+    if (numeroForMaiorouMenor(numero)) {
+        elementoChute.innerHTML += `<div>O valor informado precisa estar entre ${menorValor} e ${maiorValor}</div>`
+    }
+
+    if (numero === numeroSecreto) {
+        document.body.innerHTML = `
+            <h2>Você acertou!</h2>
+            <h3>O número secreto era ${numeroSecreto}</h3>
+
+            <button id="jogar_novamente" class="btn-jogar">Jogar novamente</button>
+        `
+    } else if(numero > numeroSecreto) {
+        elementoChute.innerHTML += `
+        <div>O número secreto é menor <i class="fa-solid fa-down-long"></i></div>
+        `
+    } else {
+        elementoChute.innerHTML += `
+        <div>O número secreto é maior <i class="fa-solid fa-up-long"></i></div>
+        `
+    }
+}
+
+function chuteForInvalido(numero) {
+    return Number.isNaN(numero);
+}
+
+function numeroForMaiorouMenor(numero) {
+    return numero > maiorValor || numero < menorValor
+}
+
+document.body.addEventListener('click', e => {
+    if (e.target.id =='jogar_novamente')
+        window.location.reload()
+})
